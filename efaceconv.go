abbd0e4fd6b6d0a7e835ad25eaa90e5d8fe13e5d
Kirill Danshin <k@guava.by>
2016-11-18 15:36:25 +0300

    add eface2byteslice

--- efaceconv.go
+++ efaceconv.go
@@ -4,17 +4,28 @@ import (
 	"unsafe"
 )
 
-var eface interface{}
+var strEface interface{}
 var strKind uintptr
+var sbEface interface{}
+var sbKind uintptr
 
 func init() {
-	eface = interface{}("")
-	strKind = *(*uintptr)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&eface)))[0]))
+	strEface = interface{}("")
+	strKind = *(*uintptr)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&strEface)))[0]))
+	sbEface = interface{}([]byte{})
+	sbKind = *(*uintptr)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&sbEface)))[0]))
 }
 
 func Eface2String(arg interface{}) (*string, bool) {
 	if *(*uintptr)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&arg)))[0])) == strKind {
 		return (*string)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&arg)))[1])), true
+	}
+	return nil, false
+}
+
+func Eface2ByteSlice(arg interface{}) (*[]byte, bool) {
+	if *(*uintptr)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&arg)))[0])) == sbKind {
+		return (*[]byte)(unsafe.Pointer((*(*[2]uintptr)(unsafe.Pointer(&arg)))[1])), true
 	}
 	return nil, false
 }
